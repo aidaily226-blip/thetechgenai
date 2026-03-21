33d52289355e9ae0ef208d563faa358fdf486474
aidaily226-blip <aidaily226@gmail.com>
2026-03-21 08:54:36 +0530

    cloudflare pages config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  reactStrictMode: true,
   images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      }
-    }
-    return config
+    domains: ['lh3.googleusercontent.com'],
   },
 }
 
