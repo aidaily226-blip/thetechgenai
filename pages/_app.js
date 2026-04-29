9db24b012e09be91514a8b76a12cd8e86c0b2264
aidaily226-blip <aidaily226@gmail.com>
2026-04-29 18:56:35 +0530

    ds

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
       </Head>
+      <Script id="infolinks-config" strategy="afterInteractive">
+        {`var infolinks_pid = 3445184; var infolinks_wsid = 0;`}
+      </Script>
+      <Script src="//resources.infolinks.com/js/infolinks_main.js" strategy="afterInteractive" />
       <Component {...pageProps} />
     </>
   )
