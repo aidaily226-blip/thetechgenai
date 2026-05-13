e59253de7acf15d247d335c34f416d0f49d53731
aidaily226-blip <aidaily226@gmail.com>
2026-05-13 12:12:15 +0530

    hu

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="google-adsense-account" content="ca-pub-4015737630275883" />
       </Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4015737630275883" strategy="afterInteractive" crossOrigin="anonymous" />
       <Script id="infolinks-config" strategy="afterInteractive">
         {`var infolinks_pid = 3445184; var infolinks_wsid = 0;`}
       </Script>
